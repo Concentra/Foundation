f0de82d2c6fd3e46d432737bf1a0319451ac24dd
Abdelrahman Hassan <aali_hassan@hotmail.com>
2013-08-31 21:37:16 +0100

    Logging Interceptor

---
+++ Foundation.Infrastructure/BL/GenericInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using Castle.DynamicProxy;
+using NHibernate;
+
+namespace Foundation.Infrastructure.BL
+{
+    class GenericInterceptor<T> : BusinessManagerInterceptor<T> where T : class, IBusinessManager
+    {
+        public GenericInterceptor(IBusinessManagerInvocationLogger businessManagerInvocationLogger, ISession session) : base(businessManagerInvocationLogger, session)
+        {
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            LogStartOfInvocation(invocation);
+            invocation.Proceed();
+            LogStartOfInvocation(invocation);
+        }
+    }
+}
