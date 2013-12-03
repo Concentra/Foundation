3c9537fcd13de9b38e554e55ec7eb7cf90018c2e
aali_hassan@hotmail.com <a40312114>
2013-12-03 09:27:36 +0000

    Donor Features

--- Foundation.Infrastructure/Notifications/NotificationHelper.cs
+++ Foundation.Infrastructure/Notifications/NotificationHelper.cs
@@ -12,7 +12,7 @@ namespace Foundation.Infrastructure.Notifications
         private readonly IEmailService emailService;
         private static readonly Dictionary<string, string> EmailConfigurations =
             ConfigurationManager.AppSettings.AllKeys
-                .Where(x => x.StartsWith("Email_"))
+                .Where(x => x.StartsWith("Foundation_Email_"))
                 .ToDictionary(x => x.Replace("Email_", string.Empty), x => ConfigurationManager.AppSettings.Get(x));
 
         public NotificationHelper(IEmailService emailService)
@@ -20,26 +20,41 @@ namespace Foundation.Infrastructure.Notifications
             this.emailService = emailService;
         }
 
-        public void SendEmail(string to, string cc, string from, string subject, string body)
+        public void SendEmail(string to, string cc, string subject, string body , string from = null)
         {
+            if (string.IsNullOrEmpty(from))
+            {
+                from = EmailConfigurations["FromAddress"];
+            }
+
             emailService.Send(to, cc, from, subject, body);
         }
 
-        public void SendEmailWithTemplateString(string to, string cc, string from, string subject, string templateContents, object templateValues)
+        public void SendEmailWithTemplateString(string to, string cc, string subject, string templateContents, object templateValues, string @from = null)
         {
+            if (string.IsNullOrEmpty(from))
+            {
+                from = EmailConfigurations["FromAddress"];
+            } 
+            
             var body = ParseText(templateContents, templateValues);
             emailService.Send(to, cc, from, subject, body);
         }
 
-        public void SendEmailWithTemplatePath(string to, string cc, string from, string subject, string templatePath, object templateValues)
+        public void SendEmailWithTemplatePath(string to, string cc, string subject, string templatePath, object templateValues, string @from = null)
         {
+            if (string.IsNullOrEmpty(from))
+            {
+                from = EmailConfigurations["FromAddress"];
+            }
+            
             string templateContents;
             using (var stream = new StreamReader(templatePath))
             {
                 templateContents = stream.ReadToEnd();
             }
 
-            SendEmailWithTemplateString(to, cc, from, subject, templateContents, templateValues);
+            SendEmailWithTemplateString(to, cc, subject, templateContents, templateValues, @from);
         }
 
         /*Utility methods for templates*/
