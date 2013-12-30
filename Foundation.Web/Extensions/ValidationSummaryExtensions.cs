2ef2f4c23825d7a1a7a8a752cfe4735bf48326ae
Abdelrahman Hassan <aali_hassan@hotmail.com>
2013-12-30 19:08:57 +0000

    bootstrap validation summary

---
+++ Foundation.Web/Extensions/ValidationSummaryExtensions.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace Foundation.Web.Extensions
+{
+    public static class ValidationSummaryExtensions
+    {
+        public static MvcHtmlString ValidationSummaryBootstrap(this HtmlHelper helper)
+        {
+            return ValidationSummaryBootstrap(helper, true);
+        }
+        
+        /// <summary>
+        /// Returns an error alert that lists each model error, much like the standard ValidationSummary only with
+        /// altered markup for the Twitter bootstrap styles.
+        /// </summary>
+        public static MvcHtmlString ValidationSummaryBootstrap(this HtmlHelper helper, bool closeable, string promptMessage = "Please fix the errors listed below and try again.")
+        {
+            # region Equivalent view markup
+            // var errors = ViewData.ModelState.SelectMany(x => x.Value.Errors.Select(y => y.ErrorMessage));
+            //
+            // if (errors.Count() > 0)
+            // {
+            //     <div class="alert alert-error alert-block">
+            //         <button type="button" class="close" data-dismiss="alert">&times;</button>
+            //         <strong>Validation error</strong> - please fix the errors listed below and try again.
+            //         <ul>
+            //             @foreach (var error in errors)
+            //             {
+            //                 <li class="text-error">@error</li>
+            //             }
+            //         </ul>
+            //     </div>
+            // }
+            # endregion
+
+            var errors = helper.ViewContext.ViewData.ModelState
+                .SelectMany(state => state.Value.Errors.Select(error => error.ErrorMessage));
+
+            int errorCount = errors.Count();
+
+            if (errorCount == 0)
+            {
+                return new MvcHtmlString(string.Empty);
+            }
+
+            var div = new TagBuilder("div");
+            div.AddCssClass("alert");
+            div.AddCssClass("fade in");
+            
+            div.AddCssClass("alert-danger");
+
+            string message = string.Empty;
+
+            if (errorCount == 1)
+            {
+                message = errors.First();
+            }
+            else
+            {
+                div.AddCssClass("alert-block");
+            }
+
+            if (closeable)
+            {
+                var button = new TagBuilder("button");
+                button.AddCssClass("close");
+                button.MergeAttribute("type", "button");
+                button.MergeAttribute("data-dismiss", "alert");
+                button.MergeAttribute("aria-hidden", "true");
+                button.SetInnerText("x");
+                div.InnerHtml += button.ToString();
+            }
+
+            div.InnerHtml += "<strong>" + promptMessage + "</strong> <br/>" +  message;
+
+            if (errorCount > 1)
+            {
+                var ul = new TagBuilder("ul");
+
+                foreach (var error in errors)
+                {
+                    var li = new TagBuilder("li");
+                    li.AddCssClass("text-error");
+                    li.SetInnerText(error);
+                    ul.InnerHtml += li.ToString();
+                }
+
+                div.InnerHtml += ul.ToString();
+            }
+
+            return new MvcHtmlString(div.ToString());
+        }
+    }
+}
